e73776018c8d273bb0efe74f943d2dfec540c5f8
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2021-10-10 03:08:31 -0600

    Finalizado el stage de objetos

---
+++ Assets/GAME/Scripts/BagStage/CheckProps.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckProps : MonoBehaviour
+{
+    public GameInstance gameStats;
+    public GameObject selector;
+    public GameObject bag;
+    public GameObject photo;
+
+    void Start()
+    {
+        Debug.Log(gameStats.levelsPassed);
+        if(gameStats.levelsPassed == 1)
+        {
+            bag.SetActive(true);
+
+        }
+        else if(gameStats.levelsPassed == 2)
+        {
+            bag.SetActive(true);
+            selector.SetActive(true);
+            photo.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
